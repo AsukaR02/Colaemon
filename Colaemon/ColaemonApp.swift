9c6d14f038d7fbb6f903418f67056a7a793c1089
AsukaR02 <459662230@qq.com>
2020-11-30 15:45:56 +0800

    Initial Commit

---
+++ Colaemon/ColaemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColaemonApp.swift
+//  Colaemon
+//
+//  Created by 赵元豪 on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct ColaemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colaemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colaemon
+//
+//  Created by 赵元豪 on 2020/11/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
